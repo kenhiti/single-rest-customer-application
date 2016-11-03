00537e280b6e9fa17e31f1440b6d3a27cd01a7ce
kenhiti <francisco.costa@adaptivesoft.com.br>
2016-11-03 20:06:46 -0200

    Atualização de classe Test

--- src/test/java/com/unitri/PersistenceTest.java
+++ src/test/java/com/unitri/PersistenceTest.java
@@ -1,12 +1,7 @@
 package com.unitri;
 
-import static org.junit.Assert.*;
-
 import java.util.List;
 
-import javax.persistence.EntityManager;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
